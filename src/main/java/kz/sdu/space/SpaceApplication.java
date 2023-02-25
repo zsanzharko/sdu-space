97fc5c9a5a349f2da51467de99c74bc2615d5ea5
Sanzhar Zhanibekov <190103039@stu.sdu.edu.kz>
2023-02-25 15:18:22 +0600

    init

---
+++ src/main/java/kz/sdu/space/SpaceApplication.java
@@ -0,0 +1,12 @@
+package kz.sdu.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpaceApplication.class, args);
+  }
+}
